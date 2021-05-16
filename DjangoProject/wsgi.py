18d1e08b925f2491a74de7c6f2facf08415b4ade
chauhan raj <chauhanraj786sinh@gmail.com>
2021-05-16 20:53:38 +0530

    dj-static

--- DjangoProject/wsgi.py
+++ DjangoProject/wsgi.py
@@ -12,5 +12,4 @@ from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
